8f089a24eadfde4045b30916ce1d42dc79b502c0
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-06-01 09:48:23 +0000

    bagan working on map of file

--- src/FileExplorer.java
+++ src/FileExplorer.java
@@ -2,20 +2,17 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 
 public class FileExplorer {
     public static void main(String... args) throws Exception {
+        HashMap<String, String> map = new HashMap<>();
         Path dir = Paths.get("./images");
-        Files.walk(dir).forEach(path -> showFile(path.toFile()));
-    }
+        Files.walk(dir) //Récupère tous les dossiers et fichiers sous ./images (un peu comme un ls -R)
+            .map(path -> path.toFile()) //Fais un tableau contenant le retour de la fonction toFile() sur chaque path
+            .filter(file -> !file.isDirectory()) //Filtre en dehors du tableau tous les fichiers étant des répertoire
+            .forEach(file -> map.put(file.getPath(), file.getName()));    //Fais une liste contenant une map (chemin du fichier -> fichier)
 
-    public static void showFile(File file) {
-        if (file.isDirectory()) {
-            System.out.println("Directory: " + file.getAbsolutePath());
-        } else {
-            System.out.println("File: " + file.getAbsolutePath());
-
-
-        }
+        System.out.println(map.size());
     }
 }
