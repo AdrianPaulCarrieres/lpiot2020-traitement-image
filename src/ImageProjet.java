bba75b2d50436a0480d7f9f14c07d4dc17175825
Clement <clement.lavallee@hotmail.com>
2021-06-08 15:25:24 +0200

    Histos, discret, normal, filtre median

--- src/ImageProjet.java
+++ src/ImageProjet.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
+import fr.unistra.pelican.ByteImage;
 import fr.unistra.pelican.Image;
 import fr.unistra.pelican.algorithms.io.ImageLoader;
-
+import fr.unistra.pelican.algorithms.visualisation.Viewer2D;
 
 public class ImageProjet {
 
@@ -26,64 +28,177 @@ public class ImageProjet {
 		return img;
 	}
 
-	public ArrayList<double[]> histogramme(Image img) {
-		ArrayList<double[]> tabHisto = new ArrayList<>();
-		if (img.getBDim() == 1) {
+	public double[][] histogramme(Image img) {
+		double[][] tabHisto = new double[3][256];
 
-			double[] tab = new double[256];
+		for (int j = 0; j < 3; ++j) {
 			for (int i = 0; i < 256; ++i) {
-				tab[i] = 0;
+				tabHisto[j][i] = 0;
 			}
-			int niveauGris;
-			for (int x = 0; x < img.getXDim(); ++x) {
-				for (int y = 0; y < img.getYDim(); ++y) {
-					niveauGris = img.getPixelXYByte(x, y);
-					tab[niveauGris] = ++tab[niveauGris];
-				}
-			}
-			tabHisto.add(tab);
-		/*
-			try {
-				HistogramTools.plotHistogram(tab);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-		} else {
-			double[] tabR = new double[256];
-			double[] tabG = new double[256];
-			double[] tabB = new double[256];
-			for (int i = 0; i < 256; ++i) {
-				tabR[i] = 0;
-				tabG[i] = 0;
-				tabB[i] = 0;
-			}
-			int couleurR, couleurG, couleurB;
-			for (int x = 0; x < img.getXDim(); ++x) {
-				for (int y = 0; y < img.getYDim(); ++y) {
-					
-					couleurR = img.getPixelXYBByte(x, y, 0);
-					couleurG = img.getPixelXYBByte(x, y, 1);
-					couleurB = img.getPixelXYBByte(x, y, 2);
-					tabR[couleurR] = ++tabR[couleurR];
-					tabG[couleurG] = ++tabG[couleurG];
-					tabB[couleurB] = ++tabB[couleurB];
-					
-					
-				}
-			}
-			tabHisto.add(tabR);
-			tabHisto.add(tabG);
-			tabHisto.add(tabB);
-			/*try {
-				HistogramTools.plotHistogramPerso(tabR, "rouge");
-				HistogramTools.plotHistogramPerso(tabG, "vert");
-				HistogramTools.plotHistogramPerso(tabB, "bleu");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
+
 		}
+
+		int couleur;
+		for (int i = 0; i < img.getBDim() && i < 3; ++i) { /* éviter le 4e channel de transparence*/
+			for (int x = 0; x < img.getXDim(); ++x) {
+				for (int y = 0; y < img.getYDim(); ++y) {
+
+					couleur = img.getPixelXYBByte(x, y, i);
+					tabHisto[i][couleur] = ++tabHisto[i][couleur];
+
+				}
+			}
+
+		}
+		/*
+		 * // valide pr RGB try { HistogramTools.plotHistogramPerso(tabHisto[0],
+		 * "rouge"); HistogramTools.plotHistogramPerso(tabHisto[1], "vert");
+		 * HistogramTools.plotHistogramPerso(tabHisto[2], "bleu"); } catch (IOException
+		 * e) {
+		 * 
+		 * e.printStackTrace(); }
+		 */
+
 		return tabHisto;
+	}
+
+	public double[][] histogrammeDiscretisé(double[][] histos) {
+		double[][] histosDiscretisés = new double[3][26];
+
+		for (int j = 0; j < 3; ++j) {
+			for (int i = 0; i < 26; ++i) {
+				histosDiscretisés[j][i] = 0;
+			}
+
+		}
+
+		for (int k = 0; k < 3; ++k) {
+			for (int i = 1; i <= 26; ++i) {
+				for (int j = 1; j <= 10; ++j) {
+					if (i * j < 256)
+						histosDiscretisés[k][i - 1] += histos[k][i * j];
+				}
+			}
+
+		}
+		/*
+		 * try { HistogramTools.plotHistogramPerso(histosDiscretisés[0], "rouge");
+		 * HistogramTools.plotHistogramPerso(histosDiscretisés[1], "vert");
+		 * HistogramTools.plotHistogramPerso(histosDiscretisés[2], "bleu"); } catch
+		 * (IOException e) {
+		 * 
+		 * e.printStackTrace(); }
+		 */
+		return histosDiscretisés;
+
+	}
+
+	public double[][] histogrammeNormalisé(double[][] histos, int len) {
+		int l = histos[0].length;
+		System.out.println(len);
+		double[][] histosNormalisés = new double[3][l];
+
+		for (int j = 0; j < 3; ++j) {
+			for (int i = 0; i < 26; ++i) {
+				histosNormalisés[j][i] = 0;
+			}
+
+		}
+
+		for (int k = 0; k < 3; ++k) {
+			for (int i = 0; i < 256; ++i) {
+				histosNormalisés[k][i] = histos[k][i] / len;
+
+			}
+
+		}
+		/*
+		 * try { HistogramTools.plotHistogramPerso(histosNormalisés[0], "rouge");
+		 * HistogramTools.plotHistogramPerso(histosNormalisés[1], "vert");
+		 * HistogramTools.plotHistogramPerso(histosNormalisés[2], "bleu"); } catch
+		 * (IOException e) {
+		 * 
+		 * e.printStackTrace(); }
+		 */
+		return histosNormalisés;
+
+	}
+
+	public Image filtreMedian(Image img) {
+
+		ByteImage new_img, new_img_bordure;
+
+		if (img.getBDim() == 1) {
+			new_img_bordure = new ByteImage(img.getXDim() + 2, img.getYDim() + 2, 1, 1, 1);
+			for (int x = 1; x < new_img_bordure.getXDim() - 1; ++x) {
+				for (int y = 1; y < new_img_bordure.getYDim() - 1; ++y) {
+
+					int couleur = (img.getPixelXYBByte(x - 1, y - 1, 0));
+					new_img_bordure.setPixelXYBByte(x, y, 0, couleur);
+				}
+			}
+			ArrayList<Integer> couleurs = new ArrayList<>();
+			new_img = new ByteImage(img.getXDim(), img.getYDim(), 1, 1, 1);
+			for (int x = 0; x < new_img.getXDim(); ++x) {
+				for (int y = 0; y < new_img.getYDim(); ++y) {
+
+					for (int i = 0; i < 3; ++i) {
+						for (int j = 0; j < 3; ++j) {
+							couleurs.add(new_img_bordure.getPixelXYByte(x + i, y + j));
+						}
+					}
+					Collections.sort(couleurs);
+					new_img.setPixelXYBByte(x, y, 0, couleurs.get(5));
+					couleurs.clear();
+				}
+			}
+
+		} else {
+			int couleurR, couleurG, couleurB;
+			System.out.println("B Dim : " + img.getBDim());
+			new_img_bordure = new ByteImage(img.getXDim() + 2, img.getYDim() + 2, 1, 1, 3);
+			for (int x = 1; x < new_img_bordure.getXDim() - 1; ++x) {
+				for (int y = 1; y < new_img_bordure.getYDim() - 1; ++y) {
+
+					couleurR = (img.getPixelXYBByte(x - 1, y - 1, 0));
+					new_img_bordure.setPixelXYBByte(x, y, 0, couleurR);
+					couleurG = (img.getPixelXYBByte(x - 1, y - 1, 1));
+					new_img_bordure.setPixelXYBByte(x, y, 1, couleurG);
+					couleurB = (img.getPixelXYBByte(x - 1, y - 1, 2));
+					new_img_bordure.setPixelXYBByte(x, y, 2, couleurB);
+				}
+			}
+			ArrayList<Integer> couleursR = new ArrayList<>();
+			ArrayList<Integer> couleursG = new ArrayList<>();
+			ArrayList<Integer> couleursB = new ArrayList<>();
+			new_img = new ByteImage(img.getXDim(), img.getYDim(), 1, 1, 3);
+			for (int x = 0; x < new_img.getXDim(); ++x) {
+				for (int y = 0; y < new_img.getYDim(); ++y) {
+
+					for (int i = 0; i < 3; ++i) {
+						for (int j = 0; j < 3; ++j) {
+							couleursR.add(new_img_bordure.getPixelXYBByte(x + i, y + j, 0));
+							couleursG.add(new_img_bordure.getPixelXYBByte(x + i, y + j, 1));
+							couleursB.add(new_img_bordure.getPixelXYBByte(x + i, y + j, 2));
+						}
+					}
+					Collections.sort(couleursR);
+					Collections.sort(couleursG);
+					Collections.sort(couleursB);
+					new_img.setPixelXYBByte(x, y, 0, couleursR.get(5));
+					new_img.setPixelXYBByte(x, y, 1, couleursG.get(5));
+					new_img.setPixelXYBByte(x, y, 2, couleursB.get(5));
+					couleursR.clear();
+					couleursG.clear();
+					couleursB.clear();
+				}
+			}
+		}
+		/*
+		boolean color2 = new_img.getBDim() == 1 ? false : true;
+		new_img.setColor(color2);
+		Viewer2D.exec(new_img);*/
+		return new_img;
+
 	}
 }
